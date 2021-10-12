8280b042b92806880804ce45c0c7401d531d2790
DESKTOP-HU5O4JL\JJ <hprimo@celerik.com>
2021-10-12 16:39:10 -0500

    Updating changes about features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import React from 'react';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Navbar from './components/Layout/Navbar';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
+   <Router>
     <div className="App">
-     <h1>Hola Mundo</h1>
+      <Navbar/>
+
+     <Switch>
+       <Route exact path= '/' component={Home}/>
+       <Route exact path= '/about' component={About}/>
+       <Route exact path= '/contact' component={Contact}/>
+       <Route component={NotFound}/>
+     </Switch>
     </div>
+   </Router>
   );
 }
 
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='container'>
+            <div className='py-4'>
+                <h1>About Page</h1>
+                <p className="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio consectetur error reiciendis. Dolor quia architecto obcaecati amet, commodi aut ducimus unde possimus distinctio natus, odit et rem harum, doloribus consectetur!
+                    </p>
+
+                <p className="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio consectetur error reiciendis. Dolor quia architecto obcaecati amet, commodi aut ducimus unde possimus distinctio natus, odit et rem harum, doloribus consectetur!
+                    </p>
+
+            </div>
+        </div>
+    );
+};
+
+export default About;
